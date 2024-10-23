a8f77b128cda81eb38c743b574bca2682cb40bc2
SteveDwumah <steve.dwmh@outlook.de>
2024-10-23 13:22:00 +0200

    DetailsPage

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -2,27 +2,27 @@
 
 const pokemonData = [
   {
-    name: 'Pikachu',
-    type: 'Electric',
-    abilities: ['Static', 'Lightning Rod'],
-    sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png'
+    name: "Pikachu",
+    type: "Electric",
+    abilities: ["Static", "Lightning Rod"],
+    sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
   },
   {
-    name: 'Charmander',
-    type: 'Fire',
-    abilities: ['Blaze', 'Solar Power'],
-    sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'
+    name: "Charmander",
+    type: "Fire",
+    abilities: ["Blaze", "Solar Power"],
+    sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
   },
 ];
 
-function PokemonDetails({ pokemon }) {
+function PokemonDetails({pokemon}) {
   return (
-    <div>
-      <h1>{pokemon.name}</h1>
-      <img src={pokemon.sprite} alt={pokemon.name} />
-      <p>Typ: {pokemon.type}</p>
-      <p>Fähigkeiten: {pokemon.abilities.join(", ")}</p>
-    </div>
+    <>
+    <h1>{pokemon.name}</h1>
+    <img src={pokemon.sprite} alt={pokemon.name} />
+    <p>Typ: {pokemon.type}</p>
+    <p>Fähigkeiten: {pokemon.abilities.join(", ")}</p>
+    </>
   );
 }
 
@@ -30,9 +30,7 @@ function PokemonList() {
   return (
     <div>
         {pokemonData.map(pokemon => (
-          <p>
-            <PokemonDetails pokemon={pokemon} />
-          </p>
+            <PokemonDetails pokemon={pokemon}/>
         ))}
     </div>
   );
