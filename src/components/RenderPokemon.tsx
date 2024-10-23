479ebe045fac2a828281989818d76be9de99537e
Robin <127445060+robin160401@users.noreply.github.com>
2024-10-23 12:37:31 +0200

    were getting data

--- src/components/RenderPokemon.tsx
+++ src/components/RenderPokemon.tsx
@@ -1,29 +1,46 @@
 import { useEffect, useState } from "react";
-import fetchAllPokemonList from "../lib/fetchPokemon";
+import { fetchAllPokemonList, fetchPokemonById } from "../lib/fetchAllPokemon";
 
 interface Pokemon {
     name: string;
     url: string;
 }
 
+
 export default function RenderPokemon() {
-    const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+    const [pokemonList, setPokemonList] = useState([]);
 
-    const fetchPokemon = async () => {
-        const result: Pokemon[] = await fetchAllPokemonList();
-        setPokemonList(result);
+    const fetchPokemonNamesAndDetails = async () => {
+        const pokemonList: Pokemon[] = await fetchAllPokemonList();
+        const detailedPokemonList = await Promise.all(
+            pokemonList.map(async (pokemon) => {
+                const details = await fetchPokemonById(pokemon.url);
+                return { ...pokemon, ...details };
+            })
+        );
+        setPokemonList(detailedPokemonList);
     };
-
+ç
     useEffect(() => {
-        fetchPokemon();
+        fetchPokemonNamesAndDetails();
     }, []);
 
-    console.log(pokemonList);
     return (
         <div>
-            
+            <h1>Pokémon List</h1>
+            <ul>
+                {pokemonList.map((pokemon, index) => (
+                    <div key={index}>
+                        <h2>{pokemon.name}</h2>
+                        <p>ID: {pokemon.id}</p>
+                        <p>Height: {pokemon.height}</p>
+                        <p>Weight: {pokemon.weight}</p>
+                    </div>
+                ))}
+            </ul>
         </div>
     );
 }
+
 
   
--- src/lib/fetchPokemon.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export default async function fetchAllPokemonList(){
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0");
-    const json = (await response.json());
-    return json;
-}
