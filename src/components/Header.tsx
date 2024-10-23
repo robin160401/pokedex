f11961c0187d647c2d50f417f97cea40adf110ef
Nana Leven <nana.leven@mail.de>
2024-10-23 10:31:49 +0200

    layout, routing, components

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+import logo from "../../public/pokeapi_logo.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="PokemonLogo" />
+      <nav>links</nav>
+      //? navlinks für "Types" und was noch?
+      <input type="text" name="search" placeholder="Search Pokemon" />
+      <button>DarkMode</button>
+      //? https://www.npmjs.com/package/react-toggle-dark-mode //? wir könnten
+      dieses package installieren!?
+    </header>
+  );
+}
