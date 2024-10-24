225e2a3391d4e2345ba1930b1dead02dd0668003
SteveDwumah <steve.dwmh@outlook.de>
2024-10-24 14:06:34 +0200

    PlaySound Cries added

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { PokemonInfos } from "../interface/PokemonDetails";
 import { fetchPokemonById } from "../lib/fetchAllPokemon";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import PlaySound from "../components/Playsound";
+
 
 function DetailsPage() {
 	const [pokemon, setPokemon] = useState<PokemonInfos>();
 	const {id} = useParams();
+	const audioRef = useRef<HTMLAudioElement>(null);
+	const [isPlaying, setIsPlaying] = useState(false);
 
 	const fetchPomemonDetailed = async () => {
 		const pokemonDetailed: PokemonInfos = await fetchPokemonById(`https://pokeapi.co/api/v2/pokemon/${id}`);
@@ -17,8 +21,19 @@ function DetailsPage() {
 		fetchPomemonDetailed();
 	}, []);
 
-	if (!pokemon)
-		return (<div>Pokemon not found</div>);
+	const handlePlayPause = () => {
+		if (isPlaying) {
+		  audioRef.current!.pause();
+		} else {
+		  const playPromise = audioRef.current!.play();
+		  if (playPromise !== undefined){
+			playPromise.catch((e) => console.error("Playback error:", e));
+		  }
+		}
+		setIsPlaying(!isPlaying);
+	  };
+		if (!pokemon)
+			return (<div>Pokemon not found</div>);
   return (
     <div>
 		<div>
@@ -26,7 +41,11 @@ function DetailsPage() {
       		<img src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name} />
       		<p>Typ: {pokemon.types[0].type.name}</p>
       		<p>Fähigkeiten: {pokemon.abilities.join(", ")}</p>
-		</div>
+			<button onClick={handlePlayPause}>
+          	{isPlaying ? "Pause" : "Play"}
+        	</button>
+        	<audio src={pokemon.cries.latest} ref={audioRef} />
+			</div>
 		<Link to="/">Back to Home</Link>
     </div>
   );
