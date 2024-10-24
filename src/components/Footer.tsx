d7c041009cc99a04916e8289f6f9f3acdf127495
SteveDwumah <steve.dwmh@outlook.de>
2024-10-24 16:22:51 +0200

    Footer added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,10 @@
 export default function Footer() {
-  return <div>Hello Footer</div>;
+  return (<footer>
+    <p>© Alle Rechte vorbehalten.</p>
+    <div className="github-link">
+    <a href="https://github.com/SteveDwumah">Steve</a>
+    <a href="https://github.com/robin160401">Robin</a>
+    <a href="https://github.com/Nana7782">Nana</a>
+    </div>
+    </footer>);
 }
