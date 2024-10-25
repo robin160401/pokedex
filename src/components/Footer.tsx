b47ceb15c1343f7a957553d61a58923010abdfcf
Nana Leven <nana.leven@mail.de>
2024-10-25 14:56:05 +0200

    pokedex

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
-  return (<footer>
-    <div className="github-link">
-    <p>GitHub:</p>
-    <a href="https://github.com/SteveDwumah">Steve</a>
-    <a href="https://github.com/robin160401">Robin</a>
-    <a href="https://github.com/Nana7782">Nana</a>
-    </div>
-    </footer>);
+  return (
+    <footer>
+      <Link to="/pokedex/1">Pokedex</Link>
+      <div className="github-link">
+        <p>GitHub:</p>
+        <a href="https://github.com/SteveDwumah">Steve</a>
+        <a href="https://github.com/robin160401">Robin</a>
+        <a href="https://github.com/Nana7782">Nana</a>
+      </div>
+    </footer>
+  );
 }
