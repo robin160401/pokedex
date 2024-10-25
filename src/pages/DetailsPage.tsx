ca5614fe0a0acb3ba15301921424f1d0476133da
Nana Leven <nana.leven@mail.de>
2024-10-25 12:51:08 +0200

    css

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -4,6 +4,7 @@ import { fetchPokemonById } from "../lib/fetchAllPokemon";
 import { useParams } from "react-router-dom";
 import playImg from "../../public/play_yellow_button_icon_227852.svg";
 import { types } from "../data/types";
+import { useThemeContext } from "../contexts/themeContext";
 
 const formatId = (id: number) => {
   return `#${String(id).padStart(3, "0")}`;
@@ -14,6 +15,7 @@ function DetailsPage() {
   const { id } = useParams();
   const audioRef = useRef<HTMLAudioElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const { theme } = useThemeContext();
 
   const fetchPomemonDetailed = async () => {
     const pokemonDetailed: PokemonInfos = await fetchPokemonById(
@@ -39,50 +41,50 @@ function DetailsPage() {
   };
   if (!pokemon) return <div>Pokemon not found</div>;
   return (
-    <div className="detailsCard">
-      <div className="details-container">
-        <div className="pokeDetail">
-          <div className="imgContainer">
-            <img
-              src={pokemon.sprites.other["official-artwork"].front_default}
-              alt={pokemon.name}
-            />
-          </div>
-          <div className="detailsText-container">
-            <div className="detailsHeading">
-              <h2>{formatId(pokemon.id)}</h2>
-              <h1>{pokemon.name}</h1>
-            </div>
-            <div className="detailsText">
-              <h2>Attacks and Movements</h2>
-              <div className="typesDiv">
-                <h3>Types </h3>
-                <div className="typesImg">
-                  {pokemon.types.map((type, index) => (
-                    <img
-                      key={index}
-                      src={types[type.type.name as keyof typeof types]}
-                      alt={type.type.name}
-                    />
-                  ))}
-                </div>
-              </div>
-
-              <p>
-                Fähigkeiten:{" "}
-                {`${pokemon.abilities[0].ability.name}, ${pokemon.abilities[1].ability.name}`}
-              </p>
-              <p>
-                Moves:{" "}
-                {`${pokemon.moves[0].move.name}, ${pokemon.abilities[1].ability.name}`}
-              </p>
-            </div>
-            <button className="playBtn" onClick={handlePlayPause}>
-              <img src={playImg} alt="" />
-            </button>
-            <audio src={pokemon.cries.latest} ref={audioRef} />
-          </div>
+    <div className="pokeDetail">
+      <div
+        className={`imgContainer ${
+          theme === "dark" ? "theme theme--dark" : "theme theme--light"
+        }`}
+      >
+        <img
+          src={pokemon.sprites.other["official-artwork"].front_default}
+          alt={pokemon.name}
+        />
+      </div>
+      <div className="detailsText-container">
+        <div className="detailsHeading">
+          <h2>{formatId(pokemon.id)}</h2>
+          <h1>{pokemon.name}</h1>
         </div>
+        <div className="detailsText">
+          <h2>Attacks and Movements</h2>
+          <div className="typesDiv">
+            <h3>Types </h3>
+            <div className="typesImg">
+              {pokemon.types.map((type, index) => (
+                <img
+                  key={index}
+                  src={types[type.type.name as keyof typeof types]}
+                  alt={type.type.name}
+                />
+              ))}
+            </div>
+          </div>
+
+          <p>
+            Fähigkeiten:{" "}
+            {`${pokemon.abilities[0].ability.name}, ${pokemon.abilities[1].ability.name}`}
+          </p>
+          <p>
+            Moves:{" "}
+            {`${pokemon.moves[0].move.name}, ${pokemon.abilities[1].ability.name}`}
+          </p>
+        </div>
+        <button className="playBtn" onClick={handlePlayPause}>
+          <img src={playImg} alt="" />
+        </button>
+        <audio src={pokemon.cries.latest} ref={audioRef} />
       </div>
     </div>
   );
